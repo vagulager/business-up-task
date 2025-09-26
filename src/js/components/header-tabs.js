25e74e9ed11eafd1f68d0f370f0a0a6c95039259
Anton <abiryukov@mbs.media>
2025-09-26 14:57:18 +0300

    fix

--- src/js/components/header-tabs.js
+++ src/js/components/header-tabs.js
@@ -1,3 +1,9 @@
 import GraphTabs from 'graph-tabs';
 
 const tabs = new GraphTabs('header-tabs');
+
+if (tabs) {
+  tabs.tabList.querySelector(
+    '.tabs__nav-item:nth-child(2) .tabs__nav-btn'
+  ).tabIndex = 0;
+}
